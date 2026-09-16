edb91ca10d1571527fcb439616f2200566b67c9c
PARAS DHALARIA <parasdhalaria2102@gmail.com>
2026-09-16 22:22:28 +0530

    learning c

---
+++ C Language/string code 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char str1[10];
+    printf("enter the string ");
+   // gets(str1);
+    //fgets(str1,sizeof(str1),stdin);
+    
+    printf ("you enter == ");
+    puts(str1);
+    return 0;
+}
